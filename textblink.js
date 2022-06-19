b1b42e83cf72fd925a4d5e0a62ee0c34ceb76a1e
careypriebe <careypriebe@github.com>
2022-06-19 23:32:40 +0545

    updates

---
+++ textblink.js
@@ -0,0 +1,11 @@
+       
+          $(function () {
+  count = 0;
+  wordsArray = ["Scientist", "Engineer", "Artist", "Statistician", "Thinker", "Researcher", "Learning","Teacher"];
+  setInterval(function () {
+    count++;
+    $("#word").fadeOut(300, function () {
+      $(this).text(wordsArray[count % wordsArray.length]).fadeIn(300);
+    });
+  }, 2000);
+});
